87d66eb89696ddd60d5d5eed729374339b6ad917
soopeach <hsjeon584@gmail.com>
2023-05-29 03:00:45 +0900

    :wrench: NetworkModule 기본 세팅

---
+++ data/src/main/java/com/soopeach/data/NetworkModule.kt
@@ -0,0 +1,45 @@
+package com.soopeach.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideClient(): Retrofit {
+
+        val clientBuilder = OkHttpClient.Builder().apply {
+            addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+        }
+
+        val contentType = "application/json".toMediaType()
+
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(Json.asConverterFactory(contentType))
+            .client(clientBuilder.build()).build()
+    }
+
+    companion object {
+        // TODO
+        private const val BASE_URL = "TODO"
+    }
+
+}
