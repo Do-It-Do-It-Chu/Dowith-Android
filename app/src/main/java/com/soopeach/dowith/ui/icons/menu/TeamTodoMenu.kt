10518f8f946f1c0fa98c97e443d16de7703acec2
soopeach <hsjeon584@gmail.com>
2023-05-30 00:30:25 +0900

    :sparkles: Bottom Navigation Icon 추가

---
+++ app/src/main/java/com/soopeach/dowith/ui/icons/menu/TeamTodoMenu.kt
@@ -0,0 +1,87 @@
+package com.soopeach.dowith.ui.icons.menu
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val TeamTodoMenu: ImageVector
+    get() {
+        if (_teamTodoMenu != null) {
+            return requireNotNull(_teamTodoMenu)
+        }
+        _teamTodoMenu = Builder(name = "TeamTodoMenu", defaultWidth = 26.0.dp, defaultHeight =
+                26.0.dp, viewportWidth = 26.0f, viewportHeight = 26.0f).apply {
+            path(fill = SolidColor(Color(0xFFB7B7B7)), stroke = null, strokeLineWidth = 0.0f,
+                    strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                    pathFillType = NonZero) {
+                moveTo(24.1429f, 3.7143f)
+                horizontalLineTo(1.8571f)
+                curveTo(1.3646f, 3.7143f, 0.8922f, 3.91f, 0.5439f, 4.2582f)
+                curveTo(0.1957f, 4.6065f, 0.0f, 5.0789f, 0.0f, 5.5714f)
+                verticalLineTo(20.4286f)
+                curveTo(0.0f, 20.9211f, 0.1957f, 21.3935f, 0.5439f, 21.7418f)
+                curveTo(0.8922f, 22.0901f, 1.3646f, 22.2857f, 1.8571f, 22.2857f)
+                horizontalLineTo(24.1429f)
+                curveTo(24.6354f, 22.2857f, 25.1078f, 22.0901f, 25.4561f, 21.7418f)
+                curveTo(25.8043f, 21.3935f, 26.0f, 20.9211f, 26.0f, 20.4286f)
+                verticalLineTo(5.5714f)
+                curveTo(26.0f, 5.0789f, 25.8043f, 4.6065f, 25.4561f, 4.2582f)
+                curveTo(25.1078f, 3.91f, 24.6354f, 3.7143f, 24.1429f, 3.7143f)
+                close()
+                moveTo(7.4286f, 16.7143f)
+                curveTo(7.3066f, 16.7144f, 7.1858f, 16.6905f, 7.0731f, 16.6439f)
+                curveTo(6.9603f, 16.5972f, 6.8579f, 16.5289f, 6.7716f, 16.4427f)
+                lineTo(3.9859f, 13.657f)
+                curveTo(3.8117f, 13.4827f, 3.7138f, 13.2464f, 3.7138f, 13.0f)
+                curveTo(3.7138f, 12.7536f, 3.8117f, 12.5173f, 3.9859f, 12.343f)
+                curveTo(4.1601f, 12.1688f, 4.3965f, 12.0709f, 4.6429f, 12.0709f)
+                curveTo(4.8893f, 12.0709f, 5.1256f, 12.1688f, 5.2998f, 12.343f)
+                lineTo(7.4286f, 14.473f)
+                lineTo(13.2716f, 8.6288f)
+                curveTo(13.3579f, 8.5425f, 13.4603f, 8.474f, 13.573f, 8.4274f)
+                curveTo(13.6857f, 8.3807f, 13.8066f, 8.3566f, 13.9286f, 8.3566f)
+                curveTo(14.0506f, 8.3566f, 14.1714f, 8.3807f, 14.2841f, 8.4274f)
+                curveTo(14.3968f, 8.474f, 14.4993f, 8.5425f, 14.5855f, 8.6288f)
+                curveTo(14.6718f, 8.715f, 14.7402f, 8.8174f, 14.7869f, 8.9302f)
+                curveTo(14.8336f, 9.0429f, 14.8577f, 9.1637f, 14.8577f, 9.2857f)
+                curveTo(14.8577f, 9.4077f, 14.8336f, 9.5286f, 14.7869f, 9.6413f)
+                curveTo(14.7402f, 9.754f, 14.6718f, 9.8564f, 14.5855f, 9.9427f)
+                lineTo(8.0855f, 16.4427f)
+                curveTo(7.9992f, 16.5289f, 7.8968f, 16.5972f, 7.7841f, 16.6439f)
+                curveTo(7.6714f, 16.6905f, 7.5505f, 16.7144f, 7.4286f, 16.7143f)
+                close()
+                moveTo(22.9427f, 9.9427f)
+                lineTo(16.4427f, 16.4427f)
+                curveTo(16.3564f, 16.529f, 16.254f, 16.5975f, 16.1413f, 16.6442f)
+                curveTo(16.0286f, 16.691f, 15.9077f, 16.715f, 15.7857f, 16.715f)
+                curveTo(15.6637f, 16.715f, 15.5429f, 16.691f, 15.4301f, 16.6442f)
+                curveTo(15.3174f, 16.5975f, 15.215f, 16.529f, 15.1287f, 16.4427f)
+                lineTo(13.2716f, 14.5855f)
+                curveTo(13.0974f, 14.4113f, 12.9995f, 14.175f, 12.9995f, 13.9286f)
+                curveTo(12.9995f, 13.6822f, 13.0974f, 13.4459f, 13.2716f, 13.2716f)
+                curveTo(13.4458f, 13.0974f, 13.6822f, 12.9995f, 13.9286f, 12.9995f)
+                curveTo(14.175f, 12.9995f, 14.4113f, 13.0974f, 14.5855f, 13.2716f)
+                lineTo(15.7857f, 14.473f)
+                lineTo(21.6287f, 8.6288f)
+                curveTo(21.715f, 8.5425f, 21.8174f, 8.474f, 21.9302f, 8.4274f)
+                curveTo(22.0429f, 8.3807f, 22.1637f, 8.3566f, 22.2857f, 8.3566f)
+                curveTo(22.4077f, 8.3566f, 22.5285f, 8.3807f, 22.6413f, 8.4274f)
+                curveTo(22.754f, 8.474f, 22.8564f, 8.5425f, 22.9427f, 8.6288f)
+                curveTo(23.029f, 8.715f, 23.0974f, 8.8174f, 23.1441f, 8.9302f)
+                curveTo(23.1908f, 9.0429f, 23.2148f, 9.1637f, 23.2148f, 9.2857f)
+                curveTo(23.2148f, 9.4077f, 23.1908f, 9.5286f, 23.1441f, 9.6413f)
+                curveTo(23.0974f, 9.754f, 23.029f, 9.8564f, 22.9427f, 9.9427f)
+                close()
+            }
+        }
+        .build()
+        return requireNotNull(_teamTodoMenu)
+    }
+
+private var _teamTodoMenu: ImageVector? = null
