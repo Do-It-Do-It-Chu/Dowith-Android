e6e45461ed0644f44fd27b0dd8914e7f54817cef
soopeach <hsjeon584@gmail.com>
2023-05-29 23:15:13 +0900

    :sparkles: DoWithColors 세팅

--- app/src/main/java/com/soopeach/dowith/ui/theme/Color.kt
+++ app/src/main/java/com/soopeach/dowith/ui/theme/Color.kt
@@ -9,3 +9,29 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+object DoWithColors {
+
+    val gray50 = Color(0xFFFCFCFC)
+    val gray100 = Color(0xFFF8F8F8)
+    val gray200 = Color(0xFFEFEFEF)
+    val gray300 = Color(0xFFDFDFDF)
+    val gray400 = Color(0xFFB7B7B7)
+    val gray500 = Color(0xFF949494)
+    val gray600 = Color(0xFF777777)
+    val gray700 = Color(0xFF555555)
+    val gray800 = Color(0xFF2A2A2A)
+    val gray900 = Color(0xFF111111)
+
+    val orange100 = Color(0xFFFFF2E5)
+    val orange200 = Color(0xFFFFE5CC)
+    val orange300 = Color(0xFFFFD9B2)
+    val orange400 = Color(0xFFFFCC99)
+    val orange500 = Color(0xFFFFBF80)
+    val orange600 = Color(0xFFFFB266)
+    val orange700 = Color(0xFFFFA54D)
+    val orange800 = Color(0xFFFF9933)
+    val orange900 = Color(0xFFFF8C1A)
+    val orange1000 = Color(0xFFFF7F00)
+
+}
