2bcc289872fd4b38e2602e120f5ded207ca73e1f
soopeach <hsjeon584@gmail.com>
2023-05-29 16:20:09 +0900

    :sparkles: 초기 bottomNavigation 세팅

--- app/src/main/java/com/soopeach/dowith/MainActivity.kt
+++ app/src/main/java/com/soopeach/dowith/MainActivity.kt
@@ -3,10 +3,7 @@ package com.soopeach.dowith
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.Modifier
+import com.soopeach.dowith.ui.AppScreen
 import com.soopeach.dowith.ui.theme.DoWithTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,13 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DoWithTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                AppScreen()
             }
         }
     }
