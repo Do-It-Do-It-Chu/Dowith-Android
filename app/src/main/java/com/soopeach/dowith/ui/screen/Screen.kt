9f094e4b220e4e6e16e9633966ef7230d7566efa
soopeach <hsjeon584@gmail.com>
2023-05-30 00:34:03 +0900

    :sparkles: Bottom Navigation 디자인 스팩에 맞춰 수정

--- app/src/main/java/com/soopeach/dowith/ui/screen/Screen.kt
+++ app/src/main/java/com/soopeach/dowith/ui/screen/Screen.kt
@@ -1,13 +1,12 @@
 package com.soopeach.dowith.ui.screen
 
 import androidx.annotation.StringRes
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Done
-import androidx.compose.material.icons.rounded.Groups
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NamedNavArgument
 import com.soopeach.dowith.R
+import com.soopeach.dowith.ui.icons.menu.DoneMenu
+import com.soopeach.dowith.ui.icons.menu.PersonalTodoMenu
+import com.soopeach.dowith.ui.icons.menu.TeamTodoMenu
 
 sealed class Screen(
     val route: String,
@@ -16,9 +15,9 @@ sealed class Screen(
     val arguments: List<NamedNavArgument> = emptyList()
 ) {
 
-    object PersonalTodo : Screen("personalTodo", R.string.personal_todo, Icons.Rounded.Person)
-    object TeamTodo : Screen("teamTodo", R.string.team_todo, Icons.Rounded.Groups)
-    object Done : Screen("done", R.string.done, Icons.Rounded.Done)
+    object PersonalTodo : Screen("personalTodo", R.string.personal_todo, PersonalTodoMenu)
+    object TeamTodo : Screen("teamTodo", R.string.team_todo, TeamTodoMenu)
+    object Done : Screen("done", R.string.done, DoneMenu)
 
     companion object {
         val bottomNavigationItems = listOf(PersonalTodo, TeamTodo, Done)
