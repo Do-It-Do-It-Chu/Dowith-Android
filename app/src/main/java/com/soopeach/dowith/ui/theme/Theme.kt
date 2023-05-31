e62c591cd131fcbba13d6243f1ca3a3137e4fb4d
soopeach <hsjeon584@gmail.com>
2023-05-31 14:44:44 +0900

    :lipstick: 상태바 흰색으로 변경

--- app/src/main/java/com/soopeach/dowith/ui/theme/Theme.kt
+++ app/src/main/java/com/soopeach/dowith/ui/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -54,8 +55,8 @@ fun DoWithTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = Color.White.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
