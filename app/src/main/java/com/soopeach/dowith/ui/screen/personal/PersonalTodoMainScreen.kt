596c8c97f5cd095745a70f2b884b72fd5d16f617
soopeach <hsjeon584@gmail.com>
2023-06-03 19:07:18 +0900

    :sparkles: 개인 Todo 메인 페이지에서 게시글 작성 기능 추가

--- app/src/main/java/com/soopeach/dowith/ui/screen/personal/PersonalTodoMainScreen.kt
+++ app/src/main/java/com/soopeach/dowith/ui/screen/personal/PersonalTodoMainScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -19,10 +23,13 @@ import com.soopeach.dowith.ui.component.button.DoWithButton
 import com.soopeach.dowith.ui.component.DoWithTopBar
 import com.soopeach.dowith.ui.component.SimplifiedTodoContainer
 import com.soopeach.dowith.ui.component.TodoItemComposable
+import com.soopeach.dowith.ui.component.bottomsheet.TodoContainerModalBottomSheetLayout
+import com.soopeach.dowith.ui.component.bottomsheet.TodoContainerModalBottomSheetType
 import com.soopeach.dowith.ui.screen.Screen
 import com.soopeach.dowith.ui.theme.DoWithColors
 import com.soopeach.dowith.viewmodel.PersonalTodoMainState
 import com.soopeach.dowith.viewmodel.PersonalTodoMainViewModel
+import kotlinx.coroutines.launch
 import org.orbitmvi.orbit.compose.collectAsState
 
 @Composable
@@ -44,56 +51,85 @@ fun PersonalTodoMainScreen(
         },
         onMoreClicked = {
             navController.navigate(Screen.PersonalTodoMore.route)
+        },
+        onCompleteButtonClicked = { todoContents ->
+            todoContents.forEach { content ->
+                viewModel.postTodoItem(content)
+            }
         }
     )
 
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PersonalTodoMainContent(
     state: PersonalTodoMainState,
     onTodoItemClicked: (Long) -> Unit = {},
-    onMoreClicked: () -> Unit = {}
+    onMoreClicked: () -> Unit = {},
+    onCompleteButtonClicked: (List<String>) -> Unit = {}
 ) {
-    Scaffold(
-        topBar = {
-            DoWithTopBar()
-        }) { paddingValues ->
 
-        Column(
-            modifier = Modifier
-                .padding(paddingValues = paddingValues)
-                .padding(20.dp)
+    val bottomSheetScope = rememberCoroutineScope()
 
-        ) {
+    val bottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = { it != ModalBottomSheetValue.HalfExpanded },
+        skipHalfExpanded = true
+    )
 
-            Box(
+    TodoContainerModalBottomSheetLayout(
+        bottomSheetType = TodoContainerModalBottomSheetType.Write,
+        coroutineScope = bottomSheetScope,
+        modalSheetState = bottomSheetState,
+        onCompleteButtonClicked = {
+            onCompleteButtonClicked(it)
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                DoWithTopBar()
+            }) { paddingValues ->
+
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(380.dp)
-                    .background(DoWithColors.gray200)
-            )
+                    .padding(paddingValues = paddingValues)
+                    .padding(20.dp)
 
-            Spacer(modifier = Modifier.height(54.dp))
+            ) {
 
-            state.todayTodoItems.getDataOrNull()?.let { todoItems ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(380.dp)
+                        .background(DoWithColors.gray200)
+                )
 
-                SimplifiedTodoContainer(
-                    onMoreClicked = onMoreClicked
-                ) {
-                    if (todoItems.isEmpty()) {
-                        DoWithButton(text = "작성하러 가기")
-                    } else {
-                        todoItems.forEach {
-                            TodoItemComposable(
-                                todoItem = it,
-                                modifier = Modifier.fillMaxWidth(),
-                                isMoreIconVisible = false,
-                                onTodoIconClicked = {
-                                    onTodoItemClicked(it.id)
+                Spacer(modifier = Modifier.height(54.dp))
+
+                state.todayTodoItems.getDataOrNull()?.let { todoItems ->
+
+                    SimplifiedTodoContainer(
+                        onMoreClicked = onMoreClicked
+                    ) {
+                        if (todoItems.isEmpty()) {
+                            DoWithButton(text = "작성하러 가기") {
+                                bottomSheetScope.launch {
+                                    bottomSheetState.show()
                                 }
-                            )
-                            Spacer(modifier = Modifier.height(16.dp))
+                            }
+                        } else {
+                            todoItems.forEach {
+                                TodoItemComposable(
+                                    todoItem = it,
+                                    modifier = Modifier.fillMaxWidth(),
+                                    isMoreIconVisible = false,
+                                    onTodoIconClicked = {
+                                        onTodoItemClicked(it.id)
+                                    }
+                                )
+                                Spacer(modifier = Modifier.height(16.dp))
+                            }
                         }
                     }
                 }
