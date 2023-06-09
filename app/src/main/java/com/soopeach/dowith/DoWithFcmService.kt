b0b4e3026d5a02a26fa099ca45fdc2ee0ee26429
soopeach <hsjeon584@gmail.com>
2023-06-09 23:00:11 +0900

    :sparkles: Head Up Notification 적용

--- app/src/main/java/com/soopeach/dowith/DoWithFcmService.kt
+++ app/src/main/java/com/soopeach/dowith/DoWithFcmService.kt
@@ -1,12 +1,12 @@
 package com.soopeach.dowith
 
-import android.app.NotificationChannel
+import android.annotation.SuppressLint
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
-import android.media.RingtoneManager
+import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -22,38 +22,46 @@ class DoWithFcmService : FirebaseMessagingService() {
         // TODO: register Token
     }
 
+    @SuppressLint("MissingPermission", "SuspiciousIndentation")
     private fun sendNotification(notification: RemoteMessage.Notification) {
         val requestCode = 0
-        val intent = Intent(this, MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
         val pendingIntent = PendingIntent.getActivity(
             this,
             requestCode,
             intent,
-            PendingIntent.FLAG_IMMUTABLE,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
         )
 
         val channelId = FCM_CHANNEL.toString()
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+
+        val notificationManager = NotificationManagerCompat.from(this)
+
+        val channel =
+            NotificationChannelCompat
+            .Builder(channelId, NotificationManager.IMPORTANCE_HIGH)
+            .setName(FCM_SERVICE_CHANNEL)
+            .build()
+
+        val notificationBuilder = NotificationCompat
+            .Builder(this, channel.id)
+            .setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
             .setSmallIcon(R.mipmap.app_ic_round)
             .setContentTitle(notification.title)
             .setContentText(notification.body)
+            .setDefaults(NotificationCompat.DEFAULT_SOUND or NotificationCompat.DEFAULT_VIBRATE)
             .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
+            .setFullScreenIntent(pendingIntent, true)
 
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        val channel = NotificationChannel(
-            channelId,
-            FCM_SERVICE_CHANNEL,
-            NotificationManager.IMPORTANCE_DEFAULT,
-        )
         notificationManager.createNotificationChannel(channel)
 
-        notificationManager.notify(FCM_CHANNEL, notificationBuilder.build())
+        notificationManager.notify(
+            (System.currentTimeMillis()).toInt(),
+            notificationBuilder.build()
+        )
     }
 
     companion object {
